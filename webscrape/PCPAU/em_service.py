c25d4f077a4ea21e46bbdaf5edbc3864900562f8
andrewapp <andrew.appuhamy@gmail.com>
2021-01-07 11:24:02 +1100

    pycharm

---
+++ webscrape/PCPAU/em_service.py
@@ -0,0 +1,13 @@
+import yagmail
+import keyring
+
+
+def sendemail(subj,body):
+    keyring.set_password('yagmail', 'aus.pcparts1@gmail.com', '301Bobby')
+    receiver = "andyapp106@gmail.com"
+    yag = yagmail.SMTP("aus.pcparts1@gmail.com")
+    yag.send(
+    to=receiver,
+    subject=subj,
+    contents=body,
+    )
